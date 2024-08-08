8baefcd34206824bb1e66e409ecab2ab1aba2871
juandad70 <juantriana716@gmail.com>
2024-08-08 16:33:34 -0500

    proyecto con formulario de registro de novedades a mi rama jdavid

---
+++ themisBackend/themis_backend/src/main/java/co/sena/edu/themis/Dto/RolesDto.java
@@ -0,0 +1,14 @@
+package co.sena.edu.themis.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class RolesDto {
+    private int id;
+    private  String name;
+}
---
+++ themisBackend/themis_backend/src/main/java/co/sena/edu/themis/ThemisApplication.java
@@ -0,0 +1,13 @@
+package co.sena.edu.themis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  ThemisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemisApplication.class, args);
+	}
+
+}
