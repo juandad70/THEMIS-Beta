3b1838bfb1ae0a3e7143fc405f6cd773263a00d0
jdavidDev <juantriana716@gmail.com>
2024-09-16 17:44:06 -0500

    fucion con el frontend envio de correos

--- themis_backend/src/main/java/co/sena/edu/themis/Controller/EmailController.java
+++ themis_backend/src/main/java/co/sena/edu/themis/Controller/EmailController.java
@@ -1,8 +1,12 @@
 package co.sena.edu.themis.Controller;
 
+import co.sena.edu.themis.Request.EmailRequest;
 import co.sena.edu.themis.Service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 @RequestMapping("/api")
@@ -12,11 +16,17 @@ public class EmailController {
     private EmailService emailService;
 
     @PostMapping("/send-notification")
-    public String sendNotification(@RequestParam String email) {
+    public String sendNotification(@RequestBody EmailRequest emailRequest) {
         try {
-            String subject = "Notificación de novedad";
-            String text = "Este es un mensaje de notificación de la novedad .";
-            emailService.sendSimpleEmail(email, subject, text);
+            // Obtener la fecha actual en formato local (DD/MM/YYYY)
+            String currentDate = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+            // Reemplazar {date} en htmlContent con la fecha actual
+            String htmlContent = emailRequest.getHtmlContent().replace("{date}", currentDate);
+
+            // Enviar el correo electrónico con el contenido HTML actualizado
+            emailService.sendHtmlEmail(emailRequest.getEmail(), emailRequest.getSubject(), htmlContent);
+
             return "Correo enviado con éxito";
         } catch (Exception e) {
             e.printStackTrace();
